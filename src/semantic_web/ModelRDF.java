890aa7e22cb8c03f6482e357cd2c0e5b47f32492
FlaviaBertoni <flavia.boava.bertoni@gmail.com>
2016-01-07 17:14:55 -0200

    Update ModelRDF.java

--- src/semantic_web/ModelRDF.java
+++ src/semantic_web/ModelRDF.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package semantic_web;
 
 import com.hp.hpl.jena.util.FileManager;
 import org.apache.jena.iri.impl.Main;
 import com.hp.hpl.jena.rdf.model.Model;
 
-/**
- *
- * @author Byzantine
- */
 public class ModelRDF {
     
      protected String file = "nomeRDF.xml";
